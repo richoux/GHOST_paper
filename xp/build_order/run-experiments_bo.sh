a910497004621761fe7e37757e51c7e199d79fa5
Florian Richoux <florian.richoux@polytechnique.edu>
2014-08-18 15:51:41 +0900

    Fix 3 dummy PvP games and add Dave's timing PvP xps

--- xp/build_order/run-experiments_bo.sh
+++ xp/build_order/run-experiments_bo.sh
@@ -5,8 +5,8 @@ do
     IFS='/' read -a filename <<< "$i"
     for k in {1..10}
     do
-	./bin/experiments_bo $i >> results/$1/"${filename[-1]}"
-	echo >> results/$1/"${filename[-1]}"
+	./bin/experiments_bo $i >> results/dave_timeouts/$1/"${filename[-1]}"
+	echo >> results/dave_timeouts/$1/"${filename[-1]}"
     done
 done
 
